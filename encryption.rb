4bc7d6fdab4b521fa02c186d49b42a310a1b30fd
APTEMmm <artemqrtz@gmail.com>
2022-07-19 00:26:30 +0300

    Added SHA2 algorithm

--- encryption.rb
+++ encryption.rb
@@ -4,7 +4,7 @@ puts 'Введите слово или фразу для шифрования:'
 user_input = gets.chomp
 
 puts 'Каким способом зашифровать:'
-puts "1. MD5 \n2. SHA1"
+puts "1. MD5 \n2. SHA1 \n3. SHA2"
 answer = gets.to_i
 
 case answer
@@ -12,6 +12,8 @@ when 1
   puts Digest::MD5.hexdigest(user_input)
 when 2
   puts Digest::SHA1.hexdigest(user_input)
+when 3
+  puts Digest::SHA2.hexdigest(user_input)
 else
   puts 'Неверный ввод'
 end
